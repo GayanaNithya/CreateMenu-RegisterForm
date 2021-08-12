4d7047bc07ecfe81c1973891dedf6f0ed39adb45
GayanaNithya <82596768+GayanaNithya@users.noreply.github.com>
2021-08-12 22:43:10 +0530

    git push b gayana

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,14 @@
 import { Routes, RouterModule } from '@angular/router';
+import { CartlistComponent } from './cartlist/cartlist.component';
 import { DummyLoginComponent } from './dummy-login/dummy-login.component';
 import { RegistercomponentComponent } from './registercomponent';
+import { WishlistComponent } from './wishlist/wishlist.component';
 
 
 const routes: Routes = [
     { path: 'register', component: RegistercomponentComponent },
-
-    // otherwise redirect to home
+    { path: 'wish_list', component: WishlistComponent },
+    { path: 'shopping_cart', component: CartlistComponent },
     { path: 'login', component: DummyLoginComponent }
 ];
 
